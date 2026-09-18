9ba1f8c1e13572066e8a2c59e74b59e49c5b9b55
vladizxc <vlad08052@gmx.de>
2026-09-18 11:52:26 +0200

    added student entity

---
+++ src/main/java/org/codekitchen/entity/Student.java
@@ -0,0 +1,89 @@
+package org.codekitchen.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="age", nullable = false)
+    private int age;
+
+    @Column(name="last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name="middle_name", length = 50)
+    private String middleName;
+
+    public Student(){}
+
+    public Student(int age, String lastName, String firstName, String middleName) {
+        this.age = age;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+    }
+
+    public Student(int age, String lastName, String firstName) {
+        this.age = age;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", age=" + age +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                '}';
+    }
+}
