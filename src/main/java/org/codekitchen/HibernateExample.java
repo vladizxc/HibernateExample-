66b54eccdeb0be571d673d8d60d042555ef11b6c
vladizxc <vlad08052@gmx.de>
2026-09-18 11:25:28 +0200

    first commit

---
+++ src/main/java/org/codekitchen/HibernateExample.java
@@ -0,0 +1,12 @@
+package org.codekitchen;
+
+/**
+ * Hello world!
+ *
+ */
+public class HibernateExample
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
